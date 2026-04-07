26e81f0f3ec545608186969a6c6ab4eae2c1a021
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-04-07 17:46:47 -0300

    Dandole estructura y clases al Title,jsx

---
+++ components/Title/Title.jsx
@@ -0,0 +1,9 @@
+
+export const Title = ({textTitle, textSpan}) => {
+
+  return (
+    <h1 className="title">{textTitle}<span className="span">{textSpan}</span></h1>
+  )
+}
+
+export default Title;
