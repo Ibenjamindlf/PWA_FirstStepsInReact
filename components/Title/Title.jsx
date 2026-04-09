9715d9d27f04680c6fef872dcd264a2ea0fa652c
Alejo <LopezAlejo0124@gmail.com>
2026-04-09 14:29:46 -0300

    Importación de los estilos y mejora de la legibilidad del código

--- components/Title/Title.jsx
+++ components/Title/Title.jsx
@@ -1,9 +1,5 @@
+import './styles.css';
 
-export const Title = ({textTitle, textSpan}) => {
-
-  return (
-    <h1 className="title">{textTitle}<span className="span">{textSpan}</span></h1>
-  )
-}
+export const Title = ({textTitle, textSpan}) => <h1 className="title">{textTitle}<span className="span">{textSpan}</span></h1>
 
 export default Title;
