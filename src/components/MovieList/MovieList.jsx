8d786a6fe7f679febc59ed62c3c8eef4e57a9895
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-04-11 18:31:27 -0300

    Añadido estilos y estructura al listado de las peliculas con sus funciones

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import styles from './MovieList.module.css';
+
+const MovieList = ({ movies: initialMovies }) => {
+    
+  const [peliculas, setPeliculas] = useState(
+    initialMovies.map(p => ({ ...p, vista: false }))
+  );
+
+  const [idArrastrado, setIdArrastrado] = useState(null);
+
+  return (
+    <section id="listado" className={styles.sectionContainer}>
+      <h2 className={styles.sectionTitle}>Mi Listado</h2>
+
+      <div className={styles.columnsContainer}>
+        
+        {/* COLUMNA IZQUIERDA (POR VER) */}
+        <div 
+          className={styles.column}
+          onDragOver={(e) => e.preventDefault()}
+          onDrop={() => {
+            setPeliculas(peliculas.map(p => p.id === idArrastrado ? { ...p, vista: false } : p));
+          }}
+        >
+          <h3 className={styles.columnTitle}>Por ver</h3>
+          <div className={styles.simpleList}>
+            {peliculas.filter(p => !p.vista).map((p) => (
+              <div 
+                key={p.id} 
+                className={styles.simpleItem}
+                draggable
+                onDragStart={() => setIdArrastrado(p.id)}
+              >
+                {p.title}
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* COLUMNA DERECHA (VISTAS) */}
+        <div 
+          className={styles.column}
+          onDragOver={(e) => e.preventDefault()}
+          onDrop={() => {
+            setPeliculas(peliculas.map(p => p.id === idArrastrado ? { ...p, vista: true } : p));
+          }}
+        >
+          <h3 className={styles.columnTitle}>Vistas</h3>
+          <div className={styles.simpleList}>
+            {peliculas.filter(p => p.vista).map((p) => (
+              <div 
+                key={p.id} 
+                className={`${styles.simpleItem} ${styles.watchedItem}`}
+                draggable
+                onDragStart={() => setIdArrastrado(p.id)}
+              >
+                {p.title}
+              </div>
+            ))}
+          </div>
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default MovieList;
